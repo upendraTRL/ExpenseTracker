e4701e40eff946abc8d2cf237f59bb96d92af8d9
upendraTRL <upendrataral21@gmail.com>
2023-09-17 13:02:02 +0530

    Dropdown button added.

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,8 +1,5 @@
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
-
-import 'package:intl/intl.dart';
-
-final formatter = DateFormat.yMd();
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -17,6 +14,7 @@ class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
   DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
 
   //Date Picker
   void _presentDatePicker() async {
@@ -90,8 +88,32 @@ class _NewExpenseState extends State<NewExpense> {
               )
             ],
           ),
+          const SizedBox(height: 16),
           Row(
             children: [
+              DropdownButton(
+                value: _selectedCategory,
+                items: Category.values
+                    .map(
+                      (category) => DropdownMenuItem(
+                        value: category,
+                        child: Text(
+                          category.name.toUpperCase(),
+                        ),
+                      ),
+                    )
+                    .toList(),
+                onChanged: (value) {
+                  print(value);
+                  if (value == null) {
+                    return;
+                  }
+                  setState(() {
+                    _selectedCategory = value;
+                  });
+                },
+              ),
+              const Spacer(),
               TextButton(
                 onPressed: () {
                   Navigator.pop(context);
