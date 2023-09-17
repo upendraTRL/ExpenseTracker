03a0e729041c8472836a304b8b37b6b41f54db6d
upendraTRL <upendrataral21@gmail.com>
2023-09-17 12:42:43 +0530

    Date picking feature, added.

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:intl/intl.dart';
+
+final formatter = DateFormat.yMd();
+
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
 
@@ -12,18 +16,26 @@ class NewExpense extends StatefulWidget {
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime? _selectedDate;
 
   //Date Picker
-  void _presentDatePicker() {
+  void _presentDatePicker() async {
     final now = DateTime.now();
     final firstDate = DateTime(now.year - 1, now.month, now.day);
 
-    showDatePicker(
+    final pickedDate = await showDatePicker(
       context: context,
       initialDate: now,
       firstDate: firstDate,
       lastDate: now,
     );
+    //Execution from here resume after await is executed(means, pickedDate is assigned with some value).
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+
+    print(pickedDate);
+    print("Selected date - " + formatter.format(_selectedDate!));
   }
 
   @override
@@ -64,7 +76,11 @@ class _NewExpenseState extends State<NewExpense> {
                   mainAxisAlignment: MainAxisAlignment.end,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    const Text('Selected Date'),
+                    Text(
+                      _selectedDate == null
+                          ? 'No Date Selected'
+                          : formatter.format(_selectedDate!),
+                    ),
                     IconButton(
                       onPressed: _presentDatePicker,
                       icon: Icon(Icons.calendar_month),
